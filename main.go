c78e91e6f762102dcba5a9d6d1d4279af280b831
Kyosuke Fujimoto <kyoro.f@gmail.com>
2022-02-13 20:50:53 +0900

    Implement entrypoint

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "os"
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/lusingander/topi/internal/openapi"
+	"github.com/lusingander/topi/internal/ui"
+)
+
+func pathFromArgs(args []string) (string, error) {
+	if len(args) <= 1 {
+		return "", errors.New("must set OpenAPI spec json/yaml filepath as argument")
+	}
+	return filepath.Abs(args[1])
+}
 
 func run(args []string) error {
-	return nil
+	path, err := pathFromArgs(args)
+	if err != nil {
+		return err
+	}
+	doc, err := openapi.Load(path)
+	if err != nil {
+		return err
+	}
+	return ui.Start(doc)
 }
 
 func main() {
