5c5b9f4cadae1cd7ce577c375c5b08db68b67d36
Kyosuke Fujimoto <kyoro.f@gmail.com>
2022-02-15 09:16:06 +0900

    Add sortPaths test

---
+++ internal/topi/topi_test.go
@@ -0,0 +1,45 @@
+package topi
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortPaths(t *testing.T) {
+	paths := []*Path{
+		{UriPath: "/abc", Method: "POST"},
+		{UriPath: "/abc/{xId}/stu", Method: "POST"},
+		{UriPath: "/abc/stu", Method: "DELETE"},
+		{UriPath: "/abc", Method: "DELETE"},
+		{UriPath: "/def/{yId}", Method: "PUT"},
+		{UriPath: "/def", Method: "POST"},
+		{UriPath: "/abc/{xId}/stu/{yId}", Method: "GET"},
+		{UriPath: "/abc", Method: "PUT"},
+		{UriPath: "/abc/{xId}", Method: "GET"},
+		{UriPath: "/abc", Method: "GET"},
+		{UriPath: "/def/{xId}", Method: "POST"},
+		{UriPath: "/abc", Method: "PATCH"},
+		{UriPath: "/abc/xyz/{xId}", Method: "PATCH"},
+		{UriPath: "/def", Method: "GET"},
+	}
+	want := []*Path{
+		{UriPath: "/abc", Method: "GET"},
+		{UriPath: "/abc", Method: "POST"},
+		{UriPath: "/abc", Method: "PUT"},
+		{UriPath: "/abc", Method: "PATCH"},
+		{UriPath: "/abc", Method: "DELETE"},
+		{UriPath: "/def", Method: "GET"},
+		{UriPath: "/def", Method: "POST"},
+		{UriPath: "/abc/{xId}", Method: "GET"},
+		{UriPath: "/abc/stu", Method: "DELETE"},
+		{UriPath: "/def/{xId}", Method: "POST"},
+		{UriPath: "/def/{yId}", Method: "PUT"},
+		{UriPath: "/abc/{xId}/stu", Method: "POST"},
+		{UriPath: "/abc/xyz/{xId}", Method: "PATCH"},
+		{UriPath: "/abc/{xId}/stu/{yId}", Method: "GET"},
+	}
+	sortPaths(paths)
+	if !reflect.DeepEqual(paths, want) {
+		t.Errorf("got=%v, want=%v", paths, want)
+	}
+}
